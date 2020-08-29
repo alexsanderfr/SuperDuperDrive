a8eb95934748c91095986f3e2badab20e12890a9
Alexsander Franco <alexsanderfrancorosa@gmail.com>
2020-08-29 18:48:10 -0300

    Add basic services

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/CredentialService.java
@@ -0,0 +1,24 @@
+package com.udacity.jwdnd.course1.cloudstorage.services;
+
+import com.udacity.jwdnd.course1.cloudstorage.mappers.CredentialMapper;
+import com.udacity.jwdnd.course1.cloudstorage.models.Credential;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CredentialService {
+    private final CredentialMapper credentialMapper;
+
+    public CredentialService(CredentialMapper credentialMapper) {
+        this.credentialMapper = credentialMapper;
+    }
+
+    public Credential getCredential(String username) {
+        return credentialMapper.selectCredential(username);
+    }
+
+    public List<Credential> getCredentials() {
+        return credentialMapper.selectAllCredentials();
+    }
+}
---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/NoteService.java
@@ -0,0 +1,20 @@
+package com.udacity.jwdnd.course1.cloudstorage.services;
+
+import com.udacity.jwdnd.course1.cloudstorage.mappers.NoteMapper;
+import com.udacity.jwdnd.course1.cloudstorage.models.Note;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class NoteService {
+    private final NoteMapper noteMapper;
+
+    public NoteService(NoteMapper noteMapper) {
+        this.noteMapper = noteMapper;
+    }
+
+    public List<Note> getNotes() {
+        return noteMapper.selectAllNotes();
+    }
+}
